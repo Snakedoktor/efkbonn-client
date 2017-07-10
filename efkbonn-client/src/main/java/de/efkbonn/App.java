aed796ccf18a8bb6f2418c6a2f86ceaf282ece70
Johnny Epp <eppjohnny@gmail.com>
2017-07-10 21:44:46 +0200

    added first javafx stage

--- efkbonn-client/src/main/java/de/efkbonn/App.java
+++ efkbonn-client/src/main/java/de/efkbonn/App.java
@@ -1,13 +1,36 @@
 package de.efkbonn;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("EFKBonn Login");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 }
